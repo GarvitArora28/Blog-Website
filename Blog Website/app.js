1d2d2fc92e120d6735ff7b43458cf8ae29b52ea4
GarvitArora28 <99586741+GarvitArora28@users.noreply.github.com>
2022-07-08 15:47:37 +0530

    Add files via upload

--- Blog Website/app.js
+++ Blog Website/app.js
@@ -1,9 +1,16 @@
-//jshint esversion:6
+//haan bhai bharosa rakh
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://excitedelectron:chhodnabro@cluster0.prkfy.mongodb.net/blogss?retryWrites=true&w=majority");
+
+const blogSchema = mongoose.Schema({title: String, body: String});
+
+const Blog = mongoose.model("Blog",blogSchema);
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,11 +23,15 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let posts = [];
-
 
 app.get('/',function(req,res){
-  res.render("home",{homeStartingContent: homeStartingContent,posts: posts});
+  Blog.find((err,blogs)=>{
+    if(err){
+      console.log(err);
+    }else{
+      res.render("home",{homeStartingContent: homeStartingContent,posts: blogs});
+    }
+  });
 });
 
 app.get('/about',function(req,res){
@@ -36,39 +47,37 @@ app.get('/compose',function(req,res){
 });
 
 app.get("/posts/:topic",function(req,res){
-  const urlParameter = _.lowerCase(req.params.topic);
-  posts.forEach(function(post){
-    const ptitle = _.lowerCase(post.title);
-    if(urlParameter === ptitle){
-      res.render("post",{post: post});
+  const urlParameter = _.startCase(_.toLower(req.params.topic));
+  Blog.findOne({title: urlParameter},(err,blog)=>{
+    res.render("post",{post: blog});
+  });
+});
+
+app.post('/compose',function(req,res){
+  const post = new Blog({
+    title: _.startCase(_.toLower(req.body.headin)),
+    body: req.body.postText
+  });
+  post.save();
+  res.redirect("/");
+});
+
+app.post('/delete',(req,res)=>{
+  const postTitle = req.body.button;
+  Blog.deleteOne({title: postTitle},(err)=>{
+    if(err){
+      console.log(err);
+    }else{
+      console.log("Successfully deleted!");
+      res.redirect("/");
     }
   });
 });
 
-// app.post("/",function(req,res){
-//   console.log(posts);
-// })
-
-app.post('/compose',function(req,res){
-  const post = {
-    title: req.body.headin,
-    body: req.body.postText
-  };
-  posts.push(post);
-  res.redirect("/");
-})
 
 
-
-
-
-
-
-
-
-
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
